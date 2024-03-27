cc45147e8d020059f02318db823cdd2e9d0a0421
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-03-27 15:20:04 -0700

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,4 @@
+function loadHome() {
+  document.title = "Chicken Wars - Home";
+  document.getElementById('content').innerHTML = "<h2>Chicken Wars Home!</h2><p>New supers update!</p><img src="src/supers.png">";
+}
