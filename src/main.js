7663c639e4ac57640f7b87d08bf4870f942cb691
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-03-27 15:30:21 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // The main code for the website
 
-function myFunc() {
+function loadHome() {
   document.title = "Chicken Wars - Home";
   document.getElementById('content').innerHTML = '<h2>Chicken Wars Home!</h2><p>New supers update!</p><img src="src/supers.png">';
 }
