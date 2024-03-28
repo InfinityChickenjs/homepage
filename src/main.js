aa8df40ac4aa550c235a4c68dfe35f76aad509f9
rubiidev18alt <164731988+rubiidev18alt@users.noreply.github.com>
2024-03-27 18:41:32 -0700

    Delete src/main.js

--- src/main.js
+++
@@ -1,6 +0,0 @@
-// The main code for the website
-
-function loadHome() {
-  document.title = "Chicken Wars - Home";
-  document.getElementById('content').innerHTML = '<h2>Chicken Wars Home!</h2><p>New supers update!</p><img src="src/supers.png">';
-}
